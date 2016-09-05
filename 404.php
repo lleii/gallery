480c50a1af8093a73f4f3bec75740abedf377d0c
lilei <lilei@live.com>
2016-09-05 22:53:02 +0800

    404 service 调试完成

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+//print_r($imgPath);
+//print_r($config['Catolog']);
+$navId='404';
+
+include 'resources/themes/uber-light/header.htm';
+include 'resources/themes/uber-light/404.htm';
+include 'resources/themes/uber-light/footer.htm';
+
+
+
+
+//echo "$config['b']['Catolog']";
+?>
---
+++ service.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+//print_r($imgPath);
+//print_r($config['Catolog']);
+$navId='ser';
+
+include 'resources/themes/uber-light/header.htm';
+include 'resources/themes/uber-light/service.htm';
+include 'resources/themes/uber-light/footer.htm';
+
+
+
+
+//echo "$config['b']['Catolog']";
+?>
