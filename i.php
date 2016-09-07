ec3796cc48d41eac2f8a2d6096e6c3ba4e85108a
lleii <lilei@live.com>
2016-09-07 10:32:08 +0800

    i.php

--- i.php
+++ i.php
@@ -1,25 +1,33 @@
 <?php
 define("LF","\r\n");
 
-//$ar2 = parse_ini_file("a.ini");
-//print_r($ar2);
+$a = parse_ini_file("a.ini");
+//print_r($argv[1]);
 
-for ($x=0; $x<=50; $x++) {
-  $ar['0000' . $x] = rand(0,10000);
+if (isset($a[$argv[1]]))
+{
+  $a[$argv[1]]++;
+}
+else
+{
+  $a[$argv[1]]=500;
+}
+arsort($a);
+print_r($a);
+//for ($x=0; $x<=50; $x++) {
+ // $ar['0000' . $x] = rand(0,10000);
   //echo "$x ==> $ar[$x]";
   //$ar2[00001] = $ar2['00001'] + 1;
-} 
+//} 
 
-  print_r($ar);
-  $s = arsort($ar);
-  print_r($ar);
+  //$s = arsort($ar);
 
 //$ar2[00001] = $ar2['00001'] + 1;
 //$ar2['00005'] =  1;
 //print_r($ar2);
 //print_r($ar3);
 //print_r($ar2['Size']);
-//write_ini_file($ar2,"a.ini");
+write_ini_file($a,"a.ini");
 
 function write_ini_file($array,$filename) {
   $ok = "";
